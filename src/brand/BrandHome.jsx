de575c20721256fe9f5f3c7c1026d72a66dd879f
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-10-20 20:59:42 +0600

    fixed Brand-Route

--- src/brand/BrandHome.jsx
+++ src/brand/BrandHome.jsx
@@ -18,9 +18,18 @@ const BrandHome = () => {
         <>
             <div className="container mx-auto" data-aos="fade-right">
                 {brand.length && <Slider items={brand[0].ads} />}
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-6 pb-6">
-                    {brand.length && brand[0].products.map(item => <ProductCard key={item._id} item={item} />)}
-                </div>
+
+                {
+                    brand[0].products.length > 0 ?
+                        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-6 pb-6">
+                            {brand[0].products.map(item => <ProductCard key={item._id} item={item} />)}
+                        </div> :
+                        <div className="px-6 pb-6 mt-6 flex w-full h-full min-h-[50dvh] justify-center items-start container mx-auto flex-col gap-4">
+                            <h3 className="font-semibold text-xl bg-yellow-300 p-12 rounded-lg shadow-lg"><span className="text-4xl">🫥</span> Hmm</h3>
+                            <h3 className="font-semibold text-xl bg-white p-12 rounded-lg shadow-lg">There is no items in here.</h3>
+                        </div>
+                }
+
 
             </div>
             <Footer />
