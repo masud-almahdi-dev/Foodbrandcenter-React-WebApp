6ee96822fffbb31c0d0efc55865895824f93c872
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-10-20 06:40:54 +0600

    Implemented signout feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,18 @@
 
-import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const location = useLocation()
+    const navigate = useNavigate()
     const [darkmode, setdarkmode] = useState(false)
     const links = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/addproduct">Add Product</NavLink></li>
         <li><NavLink to="/mycart">My Cart</NavLink></li>
-        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/login" state={location?.state ? location.state : "/"}>Login</NavLink></li>
     </>
     const toggledarkmode = () => {
         if (darkmode) {
@@ -16,7 +20,7 @@ const NavBar = () => {
             document.querySelector("body").removeAttribute("darkmode")
         } else {
             localStorage.setItem("darkmode", "true")
-            document.querySelector("body").setAttribute("darkmode","true")
+            document.querySelector("body").setAttribute("darkmode", "true")
         }
         setdarkmode(!darkmode)
         console.log(darkmode)
@@ -25,7 +29,7 @@ const NavBar = () => {
         () => {
             const isdarkmodeon = localStorage.getItem("darkmode") === "true"
             document.querySelector(".darkmodetoggle").checked = isdarkmodeon
-            document.querySelector("body").setAttribute("darkmode",localStorage.getItem("darkmode"))
+            document.querySelector("body").setAttribute("darkmode", localStorage.getItem("darkmode"))
             setdarkmode(isdarkmodeon)
 
         }, []
@@ -48,13 +52,34 @@ const NavBar = () => {
                 <ul className="menu menu-horizontal px-1 gap-2 lg:flex hidden">
                     {links}
                 </ul>
-                <NavLink to="/" className="hover:bg-white rounded-lg font-semibold text-xl items-center gap-2 transition-all hidden w-max absolute left-1/2 -translate-x-1/2 justify-center md:flex xl:text-2xl px-4 py-2 cursor-pointer">
-                    <img src="/logo2.png" className="lg:h-28" alt="" />
-                    <h2>Food Group Center</h2>
-                </NavLink>
+                {   user ?
+                    <NavLink to="/" className="hover:bg-white rounded-lg font-semibold text-xl items-center gap-2 transition-all hidden w-max absolute left-1/2 -translate-x-1/2 justify-center lg:flex xl:text-2xl px-4 py-2 cursor-pointer">
+                        <img src="/logo2.png" className="lg:h-28" alt="" />
+                        <h2>Food Group Center</h2>
+                    </NavLink>:
+                    <NavLink to="/" className="hover:bg-white rounded-lg font-semibold text-xl items-center gap-2 transition-all hidden w-max absolute left-1/2 -translate-x-1/2 justify-center md:flex xl:text-2xl px-4 py-2 cursor-pointer">
+                        <img src="/logo2.png" className="lg:h-28" alt="" />
+                        <h2>Food Group Center</h2>
+                    </NavLink>
+                }
                 <div>
-                    <h2 className="font-semibold">User</h2>
-                    <input type="checkbox" onClick={toggledarkmode} className="toggle toggle-warning darkmodetoggle" />
+                    <div className="flex gap-4 text-lg items-center text-right">
+                        <div>
+                            <div className="flex items-center justify-end gap-2 text-right">
+                                <span className="text-sm font-semibold">Dark-mode</span>
+                                <input type="checkbox" name="drktgl" onClick={toggledarkmode} className="toggle toggle-xs toggle-warning darkmodetoggle" />
+                            </div>
+                            <div className="flex justify-end text-right gap-2 items-center">
+                                {user ? <NavLink onClick={logOut} className="bg-red-500 px-2 py-1 text-white/70 text-xs rounded-md hover:bg-red-300 transition-all">Sign Out</NavLink> :
+                                    <>
+                                        <NavLink onClick={logOut} className="bg-red-500 px-2 py-1 text-white/70 text-xs rounded-md hover:bg-red-300 transition-all" to="/login" state={location?.state ? location.state : "/"}>Sign In</NavLink>
+                                        <NavLink onClick={logOut} className="bg-red-500 px-2 py-1 text-white/70 text-xs rounded-md hover:bg-red-300 transition-all" to="/signup" state={location?.state ? location.state : "/"}>Sign Up</NavLink>
+                                    </>}
+                                {user && user.displayName ? <h2>{user.displayName}</h2> : <h2>No Name</h2>}</div>
+                            <div className="text-xs text-white/70 bg-red-500 px-2 py-1">{user ? user.email : "Not Logged In"}</div>
+                        </div>
+                        <div>{user ? user.photoURL ? <img src={user.photoURL} className="w-20 h-20 object-cover overflow-hidden aspect-square rounded-lg" alt="" /> : <img className="w-20 h-20 object-cover overflow-hidden aspect-square rounded-lg" src="https://i.ibb.co/sKbWp7W/21104.jpg" alt="" /> : <></>}</div>
+                    </div>
                 </div>
             </div>
         </div>
