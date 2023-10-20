ca0b208c846dda61fa04bf5812ee7b0aaeb2f591
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-10-20 21:29:28 +0600

    added 2 extra sections in home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -20,12 +20,27 @@ const Home = () => {
                         <h4 className="bg-yellow-400 rounded-md py-1 font-semibold px-2 mr-[10dvh] text-xs md:text-sm lg:text-base md:mr-32">Fast food and cold drinks that will make you come back for more. Designed to satisfy your cravings. Whether you’re in the mood for a burger, fries, or a refreshing drink, we’ve got you covered</h4>
                     </div>
                 </div>
+
                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
                     {
                         brands.map(
                             item => <BrandItem key={item._id} brand={item} />
                         )
                     }
+                </div>
+                <div className="w-full my-10 flex items-center justify-center" data-aos="fade-right">
+                    <img src="https://i.ibb.co/FXgDWhS/facts.jpg" className="object-cover w-full md:aspect-[2/1] aspect-[2/1] overflow-hidden rounded-lg" alt="" />
+                    <div className="absolute m-2 mr-6 gap-2 flex flex-col justify-center items-center">
+                        <h2 className="bg-white text-xl md:text-3xl xl:text-4xl rounded-md py-1 px-2 lg:py-2 lg:px-4 md:mx-32 w-max">Did you know!</h2>
+                        <h4 className="bg-white rounded-md py-1 font-semibold px-2 mr-[10dvh] text-xs md:text-sm lg:text-base md:mx-32">Sturbucks have over 1500 stores in America alone!</h4>
+                    </div>
+                </div>
+                <div className="w-full mt-4 flex items-center justify-center" data-aos="fade-right">
+                    <img src="https://i.ibb.co/SJspTLJ/fast-food.jpg" className="object-cover w-full md:aspect-[3/1] aspect-[2/1] overflow-hidden rounded-lg" alt="" />
+                    <div className="absolute m-2 mr-6 gap-2 flex flex-col justify-center items-center">
+                        <h2 className="bg-yellow-400 text-xl md:text-3xl xl:text-4xl rounded-md py-1 px-2 lg:py-2 lg:px-4 md:mx-32 w-max">Awesome taste That will never leave you!</h2>
+                        <h4 className="bg-yellow-400 rounded-md py-1 font-semibold px-2 mr-[10dvh] text-xs md:text-sm lg:text-base md:mx-32">Fast food and cold drinks that will make you come back for more. Designed to satisfy your cravings. Whether you’re in the mood for a burger, fries, or a refreshing drink, we’ve got you covered</h4>
+                    </div>
                 </div>
             </div>
             <Footer></Footer>
